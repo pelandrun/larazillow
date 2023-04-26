407c0f1d471d603157f06276f3de797e549ec819
root <root@DESKTOP-H1FNBRF>
2023-04-26 18:05:07 -0300

    2

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserAccountController;
+use App\Http\Controllers\RealtorListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +28,13 @@ Route::delete('logout', [AuthController::class, 'destroy'])
   ->name('logout');
   Route::resource('user-account', UserAccountController::class)
     ->only(['create','store']);
+
+    Route::prefix('realtor')
+    ->name('realtor.')
+    ->middleware('auth')
+    ->group(function () {
+      Route::resource('listing', RealtorListingController::class);
+    });
 //   ->only(['index', 'show','create','store','edit']);
 // Route::get('/', function () {
 //     return view('app');
